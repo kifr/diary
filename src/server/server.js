0572ce00c69456c920e5fc7b64143bbcbfe3cea1
K-Twig <27399981+K-Twig@users.noreply.github.com>
2020-04-07 19:02:29 +0900

    create docker environment for using mongoDB

--- src/server/server.js
+++ src/server/server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
+const databaseUrl = process.env.MONGO_DATABASE || '';
+mongoose.connect(databaseUrl, (err, client) => {
+  if (err) console.error(err);
+});
 
 const server = express();
-
 server.use(express.static(path.join('./', 'dist')));
 
 server.get('/', (req, res) => {
   res.sendFile(path.join('./', 'dist', 'index.html'));
-})
+});
 
 server.listen(3000, () => {
   console.log('server running');
